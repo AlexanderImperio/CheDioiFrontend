6901d6b039927516faaf35b7c26b970619e62943
Thiago Augusto <thiagoggth@gmail.com>
2020-01-15 15:25:31 -0400

    Implementada validaçoes de update.

--- CheDioiFrontend/WebAppFrontEnd/Projeto/controller/areasDeConhecimento/controllerRead.js
+++ CheDioiFrontend/WebAppFrontEnd/Projeto/controller/areasDeConhecimento/controllerRead.js
@@ -4,33 +4,32 @@ myControllers.controller('controllerRead', ['$rootScope', '$scope', '$http', '$l
     function ($rootScope, $scope, $http, $location) {
 
         $rootScope.areasDeConhecimento = [
-            { idArea: 1, descricao: "Ciências Exatas e da Terra"},
-            { idArea: 2, descricao: "Matemática"},
-            { idArea: 3, descricao: "Álgebra"},
-            { idArea: 4, descricao: "Conjuntos"},
-            { idArea: 5, descricao: "Lógica Matemática"},
-            { idArea: 6, descricao: "Teoria dos Números"},
-            { idArea: 7, descricao: "Grupos de Álgebra Não - Comutaviva"},
-            { idArea: 8, descricao: "Álgebra Comutativa"},
-            { idArea: 9, descricao: "Geometria Algébrica"},
-            { idArea: 10, descricao: "Análise"},
-            { idArea: 11, descricao: "Análise Complexa"},
-            { idArea: 12, descricao: "Análise Funcional"},
-            { idArea: 13, descricao: "Análise Funcional Não - Linear"},
-            { idArea: 14, descricao: "Equações Diferenciais Ordinárias"},
-            { idArea: 15, descricao: "Equações Diferenciais Parciais"},
-            { idArea: 16, descricao: "Equações Diferenciais Funcionais"},
-            { idArea: 17, descricao: "Geometria e Topologia"},
-            { idArea: 18, descricao: "Geometria Diferencial"},
-            { idArea: 19, descricao: "Topologia Algébrica"},
-            { idArea: 20, descricao: "Topologia das Variedades"},
+            { idArea: 1, descricao: "Ciências Exatas e da Terra" },
+            { idArea: 2, descricao: "Matemática" },
+            { idArea: 3, descricao: "Álgebra" },
+            { idArea: 4, descricao: "Conjuntos" },
+            { idArea: 5, descricao: "Lógica Matemática" },
+            { idArea: 6, descricao: "Teoria dos Números" },
+            { idArea: 7, descricao: "Grupos de Álgebra Não - Comutaviva" },
+            { idArea: 8, descricao: "Álgebra Comutativa" },
+            { idArea: 9, descricao: "Geometria Algébrica" },
+            { idArea: 10, descricao: "Análise" },
+            { idArea: 11, descricao: "Análise Complexa" },
+            { idArea: 12, descricao: "Análise Funcional" },
+            { idArea: 13, descricao: "Análise Funcional Não - Linear" },
+            { idArea: 14, descricao: "Equações Diferenciais Ordinárias" },
+            { idArea: 15, descricao: "Equações Diferenciais Parciais" },
+            { idArea: 16, descricao: "Equações Diferenciais Funcionais" },
+            { idArea: 17, descricao: "Geometria e Topologia" },
+            { idArea: 18, descricao: "Geometria Diferencial" },
+            { idArea: 19, descricao: "Topologia Algébrica" },
+            { idArea: 20, descricao: "Topologia das Variedades" },
         ];
 
         $scope.modalDelete = {
-            template:"modalDelete.html"
-        };       
+            template: "modalDelete.html"
+        }
 
-   
     }
 ]);
 
--- CheDioiFrontend/WebAppFrontEnd/Projeto/controller/areasDeConhecimento/controllerUpdate.js
+++ CheDioiFrontend/WebAppFrontEnd/Projeto/controller/areasDeConhecimento/controllerUpdate.js
@@ -2,52 +2,69 @@
 
 myControllersUpdate.controller('controllerUpdate', ['$rootScope', '$scope', '$http', '$location',
     function ($rootScope, $scope, $http, $location) {
+        $scope.descricao;
+
         $scope.modalUpdate = {
-            template:"modalUpdate.html"
+            template: "modalUpdate.html"
         };
 
-        $rootScope.modalUpdate = function (municipio){            
-            $rootScope.municipioAtual = municipio;                        
-            $('#modal-update').modal('show'); }
-        
-        $scope.update = function (estado) {
-            let municipioAtual = $rootScope.municipioAtual.nome;
-            let municipio = document.getElementById("nomeMunicipioUpdate").value;            
-            let error = false;            
-            let keepGoing = true;
+        $rootScope.modalUpdate = function (atual) {
+            $rootScope.areaConhecimentoAtual = atual;
+            $scope.descricao = atual.descricao;
+            $('#modal-update').modal('show');
+        }
 
-            if (municipio == "" || estado == null) {
-                if (municipio == "") {
-                    nomeVazil()
-                }
-                if (estado == null){
-                    seletorVazil();
-                }                
-            } else {
-                angular.forEach($rootScope.municipios, function (value) {
-                    if (keepGoing) {
-                        if (value.id == $rootScope.municipioAtual.id) {
-                            if (value.nome == municipio && value.microrregiao.mesorregiao.UF.sigla == estado.sigla) {
-                                nomeIgual()
-                            }
-                            else {
-                                value.nome = municipio;
-                                value.microrregiao.mesorregiao.UF.sigla = estado.sigla;
-                                value.microrregiao.mesorregiao.UF.nome = estado.nome;
-                                showUpdateMessage(false, municipioAtual);
-                            }
-                            keepGoing = false;
-                            error = false;
-                        } else {
-                            error = true;
-                        }
+        $scope.update = function (areaConhecimento) {
+
+            //validando para ver se esta vazil!.
+            if (areaConhecimento) {
+                const areaConhecimentoArr = areaConhecimento.split(' ').map(a => a.trim());
+
+                let removeSpaço;
+
+                areaConhecimentoArr.forEach(element => {
+                    if (element.length > 3) {
+                        element = element.charAt(0).toUpperCase() + element.slice(1);
                     }
-                });
 
-                if (error) {
-                    showUpdateMessage(error, municipio);
+                    if (removeSpaço == null) {
+                        removeSpaço = element;
+                    } else {
+                        removeSpaço += ' ' + element;
+                    }
+
+                });
+                areaConhecimento = removeSpaço;
+
+                /* Função "validacao()" para verificar se ja existe 
+                *  um registro igual ao que esta sendo cadastrado.
+                **/
+                let validacao = () => {
+                    let retorno = true;
+                    $rootScope.areasDeConhecimento.forEach(element => {
+                        if (areaConhecimento.toLowerCase() == element.descricao.toLowerCase()) {
+                            alert('Já existe uma area de conhecimento cadastrada com esta descrição');
+                            retorno = false;
+                        }
+                    });
+
+                    return retorno
                 }
-            }            
+
+                //Se tiver tudo serto inserir novo registro no array.
+                if (validacao()) {
+                    $rootScope.areasDeConhecimento.forEach((element, index) => {
+
+                        if (element.idArea == $rootScope.areaConhecimentoAtual.idArea) {
+                            $rootScope.areasDeConhecimento[index].descricao = areaConhecimento;
+                        }
+                    });
+                    alert('Alteração realizada com sucesso!');
+                }
+
+            } else {
+                alert('Campo descrição obrigadorio!');
+            }
         }
     }
 ]);
