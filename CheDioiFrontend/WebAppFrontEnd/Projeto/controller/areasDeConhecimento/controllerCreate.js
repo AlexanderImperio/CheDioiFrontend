c464362876c421667ce45f0eb1ffc6e240837168
Thiago Augusto <thiagoggth@gmail.com>
2020-01-15 13:18:39 -0400

    Implementado as validações para criar uma nova area de conhecimento.

--- CheDioiFrontend/WebAppFrontEnd/Projeto/controller/areasDeConhecimento/controllerCreate.js
+++ CheDioiFrontend/WebAppFrontEnd/Projeto/controller/areasDeConhecimento/controllerCreate.js
@@ -2,7 +2,7 @@
 
 myControllerCreate.controller('controllerCreate', ['$rootScope', '$scope', '$http', '$location',
     function ($rootScope, $scope, $http, $location) {
-
+        $scope.descricao = "";
         //para incluir a pagina modalCreate na tela de lista de municipios
         $scope.modalCreate = {
             template: "modalCreate.html"
@@ -13,65 +13,57 @@ myControllerCreate.controller('controllerCreate', ['$rootScope', '$scope', '$htt
             $('#modal-create').modal('show');
         }
 
-        //variavel para criar novos Id's para municipio.
-        let idMunicipio = 1;
 
-        //funcação para criar novo municipio.
-        $scope.removeInput = function (selecaoEstado) {
-            let contMunicipios = $rootScope.municipios.length;
-            let Municipio = document.getElementById("nomeMunicipio").value;            
-            let municipioEstado; 
-            let municipioEstadoSigla;
-            let keepGoing = true;
-            let igual = false;
+        $scope.create = function (areaConhecimento) {
+            //validando para ver se esta vazil!.
+            if (areaConhecimento) {
+                const areaConhecimentoArr = areaConhecimento.split(' ').map(a => a.trim());
 
-            //validação para ver se o campo esta vazil.
-            if (selecaoEstado == null || Municipio == "") {
-                if (selecaoEstado == null) {
-                    seletorVazil();
-                }
-                if (Municipio == "") {
-                    nomeVazil();
-                }
-                
-            } else {
-                municipioEstado = selecaoEstado.id;
-                municipioEstadoSigla = selecaoEstado.sigla;
-                 
-                angular.forEach($rootScope.municipios, function (value) {
-                    if (keepGoing) {
-                        if (value.nome.toLowerCase() == Municipio.toLowerCase() && value.microrregiao.mesorregiao.UF.sigla == municipioEstadoSigla) {
-                            nomeCreateIgual()
-                            igual = true;
-                            keepGoing = false;
-                        }
+                let removeSpaço;
+
+                areaConhecimentoArr.forEach(element => {
+                    if (element.length > 3) {
+                        element = element.charAt(0).toUpperCase() + element.slice(1);
                     }
+
+                    if (removeSpaço == null) {
+                        removeSpaço = element;
+                    } else {
+                        removeSpaço += ' ' + element;
+                    }
+
                 });
+                areaConhecimento = removeSpaço;
 
-                if (igual == false) {
-                    idMunicipio += 1;
-                    let municipioList = {
-                        'id': contMunicipios + idMunicipio,
-                        'nome': Municipio,
-                        'microrregiao': {
-                            'mesorregiao': {
-                                'UF': {
-                                    'sigla': municipioEstadoSigla,
-                                    'nome': municipioEstado
-                                }
-                            }
+                /* Função "validacao()" para verificar se ja existe 
+                *  um registro igual ao que esta sendo cadastrado.
+                **/
+                let validacao = () => {
+                    let retorno = true;
+                    $rootScope.areasDeConhecimento.forEach(element => {
+                        if (areaConhecimento.toLowerCase() == element.descricao.toLowerCase()) {
+                            alert('Esta area do conhecimento ja existe');
+                            retorno = false;
                         }
+                    });
+
+                    return retorno
+                }
+
+                //Se tiver tudo serto inserir novo registro no array.
+                if (validacao()) {
+                    const cad = {
+                        idArea: $rootScope.areasDeConhecimento.length + 1,
+                        descricao: areaConhecimento
                     }
+                    $rootScope.areasDeConhecimento.push(cad);
+                }
+                $scope.descricao = "";
 
-                    $rootScope.municipios.push(municipioList);
-                    $('#modal-create .modal-body input[type=text]').prop('value', '');                    
-                    showInputMessage(false, Municipio);
-                    keepGoing = false;
-                }                
-
+            } else {
+                //caso teja vazil.
+                alert('compo vazil');
             }
-            
         }
-    
     }
 ]);
