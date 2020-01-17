098165c405c69c23f6976084c0d92363d82d80f1
Alexander Imperio <alexanderodrigues22@hotmail.com>
2020-01-17 00:14:10 -0300

    adicionado arquivos de profissoes

---
+++ CheDioiFrontend/WebAppFrontEnd/Projeto/controller/profissoes/controllerDelete.js
@@ -0,0 +1,23 @@
+﻿var myControllersProfissoesDelete = angular.module("myControllersProfissoesDelete", []);
+
+myControllersProfissoesDelete.controller('profissaoControllerDelete', ['$rootScope', '$scope', '$http', '$location',
+    function ($rootScope, $scope, $http, $location) {
+        $scope.modalDelete = {
+            template: "modalDelete.html"
+        };
+
+        $rootScope.modalDelete = function (profissao) {
+            $rootScope.profissaoAtual = profissao;
+            $('#modal-delete').modal('show');
+            $scope.descricaoProfissao = $rootScope.profissaoAtual.Descricao;
+
+        }
+
+
+
+        $scope.fakeDelete = (profissaoAtual) => {
+            $('#remove-' + profissaoAtual.IdProfissao).hide();
+            messageDelete(false, $rootScope.profissaoAtual.Descricao);
+        }
+
+    }]);
