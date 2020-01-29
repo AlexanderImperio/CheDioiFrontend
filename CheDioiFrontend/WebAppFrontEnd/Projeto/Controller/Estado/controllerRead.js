83bba62537e39bb06c9ede3023da0b5e7c1bfe17
Thiago Augusto <thiagoggth@gmail.com>
2020-01-28 23:18:07 -0400

    Configurado para nao recarregar o array de estado

--- CheDioiFrontend/WebAppFrontEnd/Projeto/Controller/Estado/controllerRead.js
+++ CheDioiFrontend/WebAppFrontEnd/Projeto/Controller/Estado/controllerRead.js
@@ -2,37 +2,41 @@
 
 myControllers.controller('controllerRead', ['$rootScope', '$scope', '$http', '$location',
     function ($rootScope, $scope, $http, $location) {
-                var uri = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
+        var uri = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
 
-                $http.get(uri).then(function (retorno) {
-                    $rootScope.Estados = retorno.data;
-                });
-                $rootScope.estadoSelecionado;
+        $rootScope.Estados;
 
-                $scope.deletar = function (estado) {
-                    $rootScope.estadoSelecionado = estado;
-                    $location.path('/deletar');
+        if (!$rootScope.Estados) {
+            $http.get(uri).then(function (retorno) {
+                $rootScope.Estados = retorno.data;
+            });
+        }
 
-                };
+        $rootScope.EstadoSelecionado;
 
-                $scope.atualizar = function (estado) {
-                    $rootScope.estadoSelecionado = estado;
-                    $location.path('/atualizar');
-
-                };
-
-                $scope.criar = function () {
-                    $location.path('/criar');
-                };
-
-                $scope.voltar = function () {
-                    $location.path('/menu');
-                };
-
-                $scope.sort = function (keyname) {
-                    $scope.sortKey = keyname;
-                    $scope.reverse = !$scope.reverse;
-                }
+        $scope.Deletar = function (estado) {
+            if (confirm("Deseja Realmente deletar este registro?")) {
+                
             }
-        ]);
-    
+        };
+
+        $scope.atualizar = function (estado) {
+            $rootScope.estadoSelecionado = estado;
+            $location.path('/atualizar');
+
+        };
+
+        $scope.criar = function () {
+            $location.path('/criar');
+        };
+
+        $scope.voltar = function () {
+            $location.path('/menu');
+        };
+
+        $scope.sort = function (keyname) {
+            $scope.sortKey = keyname;
+            $scope.reverse = !$scope.reverse;
+        }
+    }
+]);
