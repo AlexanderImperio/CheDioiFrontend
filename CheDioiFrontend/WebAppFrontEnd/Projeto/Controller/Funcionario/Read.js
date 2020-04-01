6e320d7654459ff316fc3f125a013e47575935d1
Thiago Augusto <thiagoggth@gmail.com>
2020-04-01 19:33:39 -0400

    Implementado tela de listagem de funcionarios.

--- CheDioiFrontend/WebAppFrontEnd/Projeto/Controller/Funcionario/Read.js
+++ CheDioiFrontend/WebAppFrontEnd/Projeto/Controller/Funcionario/Read.js
@@ -1,1 +1,11 @@
-﻿
+﻿var myControllers = angular.module("ReadController", []);
+
+myControllers.controller('ReadController', ['$rootScope', '$scope', '$http', '$location',
+    async function ($rootScope, $scope, $http, $location) {
+
+        $scope.funcrionarios = [1,2,3,4,5,6,7,8,9,10];
+
+        
+    }
+]);
+
--- CheDioiFrontend/WebAppFrontEnd/Projeto/Module/Funcionario/ModuleFuncionario.js
+++ CheDioiFrontend/WebAppFrontEnd/Projeto/Module/Funcionario/ModuleFuncionario.js
@@ -1,1 +1,7 @@
-﻿
+﻿var myApp = angular.module('myApp', ['ngRoute', 'ControllerHeader', 'ReadController']);
+
+myApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/funcionarios', { templateUrl: endereco + 'Funcionario/funcionario.html' }).
+        otherwise({ redirectTo: '/funcionarios' });
+}]);
