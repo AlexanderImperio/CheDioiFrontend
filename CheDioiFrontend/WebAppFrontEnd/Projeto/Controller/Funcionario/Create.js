6d0cae947f21cd188e9072e306a0df5f94eb9bb2
Thiago Augusto <thiagoggth@gmail.com>
2020-04-03 13:33:30 -0400

    Adicionado o controller de create a lista de constrollers de funcionarios.

--- CheDioiFrontend/WebAppFrontEnd/Projeto/Controller/Funcionario/Create.js
+++ CheDioiFrontend/WebAppFrontEnd/Projeto/Controller/Funcionario/Create.js
@@ -1,1 +1,8 @@
-﻿
+﻿var myControllers = angular.module("ControllerCreate", []);
+
+myControllers.controller('ControllerCreate', ['$rootScope', '$scope', '$http', '$location',
+    async function ($rootScope, $scope, $http, $location) {
+        $scope.test = 'teste';
+    }
+]);
+
--- CheDioiFrontend/WebAppFrontEnd/Projeto/Module/Funcionario/ModuleFuncionario.js
+++ CheDioiFrontend/WebAppFrontEnd/Projeto/Module/Funcionario/ModuleFuncionario.js
@@ -1,4 +1,4 @@
-﻿var myApp = angular.module('myApp', ['ngRoute', 'ControllerHeader', 'ControllerRead']);
+﻿var myApp = angular.module('myApp', ['ngRoute', 'ControllerHeader', 'ControllerRead', 'ControllerCreate']);
 
 myApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
