c412797141e9ab9b3b5184157451378c68fb9954
Alexander Imperio <alexanderodrigues22@hotmail.com>
2020-04-05 21:52:12 -0400

    Criado metodo de salvar com dados prenchidos do html no JS da tela Cadastrar Funcionario

--- CheDioiFrontend/WebAppFrontEnd/Projeto/Controller/Funcionario/Create.js
+++ CheDioiFrontend/WebAppFrontEnd/Projeto/Controller/Funcionario/Create.js
@@ -2,7 +2,84 @@
 
 myControllers.controller('ControllerCreate', ['$rootScope', '$scope', '$http', '$location',
     async function ($rootScope, $scope, $http, $location) {
-        $scope.hellow = 'Hellow to create controller';
+
+        $scope.Cadastrar = function () {
+            const NovoCadastro = {
+                Endereco: {
+                    IdEndereco: null,
+                    IdMunicipio: null,
+                    CEP: $scope.Cep,
+                    Bairro: $scope.Bairro,
+                    Logradouro: $scope.Logradouro,
+                    Numero: $scope.Numero,
+                    Complemento: $scope.Complemento
+                },
+                Dependente: {
+                    IdDependente: null,
+                    IdEndereco: null,
+                    Nome: $scope.NomeDependente,
+                    CertidaoDeNascimento: $scope.CertidaoNascimento,
+                    Rg: $scope.RgDependente,
+                    OrgaoEmissor: $scope.OrgaoDependente,
+                    Cpf: $scope.CpfDependente,
+                    DataNascimento: $scope.DataNascimentoDependente,
+                    Sexo: $scope.SexoDependente,
+                    GrauParentesco: $scope.GrauParentesco
+                },
+                Recibo: {
+                    IdRecibo: null,
+                    IdFuncionario: null,
+                    DataPadrao: $scope.DataPadrao,
+                    ValorInicial: $scope.ValorInicial,
+                    ValorAtual: $scope.ValorAtual,
+                    Pagamento_Adiantamento: null,
+                    ValorFerias: $scope.ValorFerias
+                },
+                Reajuste: {
+                    IdReajuste: null,
+                    Reajuste_SalarialData: $scope.Reajuste_SalarialData,
+                    Reajuste_SalarialValor: $scope.Reajuste_SalarialValor,
+                    Reajuste_SindicalData: $scope.Reajuste_SindicalData,
+                    Reajuste_SindicalValor: $scope.Reajuste_SindicalValor:
+                },
+                Contrato: {
+                    IdContrato: null,
+                    IdFuncionario: null,
+                    IdReajuste: null,
+                    StatusAtual: $scope.StatusAtual,
+                    DataDeAdimissao: $scope.DataDeAdimissao,
+                    DataRecisao: $scope.DataRecisao
+                },
+                IdFuncionario: null,
+                IdDependente: null,
+                IdEndereco: null,
+                IdProfissao: null,
+                IdAreaDeConhecimento: null,
+                Matricula: $scope.Matricula,
+                Nome: $scope.Nome,
+                Rg: $scope.Rg,
+                OrgaoEmissor: $scope.OrgaoEmissor,
+                Cpf: $scope.Cpf,
+                Cnh: $scope.Cnh,
+                DataNascimento: $scope.DataNascimento,
+                Telefone: $scope.Telefone,
+                Celular: $scope.Celular,
+                Email: $scope.Email,
+                EstadoCivil: $scope.EstadoCivil,
+                Sexo: $scope.Sexo,
+                Ctps: $scope.Ctps,
+                CtpsSerie: $scope.CtpsSerie,
+                CtpsUf: $scope.CtpsUf,
+                Pispasep: $scope.Pispasep,
+                Interesses: $scope.Interesses,
+                Banco: $scope.Banco,
+                ContaBancaria: $scope.ContaBancaria,
+                TipoDeConta: $scope.TipoDeConta,
+                AgenciaBancaria: $scope.AgenciaBancaria,
+                Salario: $scope.Salario
+            };
+            console.log(NovoCadastro);
+        };
     }
 ]);
 
